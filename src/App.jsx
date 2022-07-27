b3e699f4b1390883a9e5eb949f447d6559eb0d48
Mariia Shabelnik <>
2022-07-27 18:34:46 +0200

    add hamb_menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Experiance from "./pages/Experiance";
+import Experience from "./pages/Experience";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -12,7 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="experiance" element={<Experiance />} />
+        <Route path="experience" element={<Experience />} />
         <Route path="contact" element={<Contact />} />
       </Routes>
       <Footer />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,49 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Turn as Hamburger } from "hamburger-react";
+import { useState } from "react";
 
 function Header() {
+  const [isOpen, setOpen] = useState(false);
+
+  const hamburgerMenu = (
+    <div className="hamburger-menu">
+      <div>
+        <Link to="/">.me()</Link>
+      </div>
+      <div>
+        <Link to="/experience">.experience()</Link>
+      </div>
+      <div>
+        <Link to="/contact">.contact()</Link>
+      </div>
+    </div>
+  );
+
   return (
     <div className="nav">
-      <div className="container nav-items">
-        <div className="logo">
-          <Link to="/">LOGO</Link>
+      <div className="container">
+        <div className=" nav-items">
+          <div className="logo">
+            <Link to="/">MS.</Link>
+          </div>
+          <div className="hamburger">
+            <Hamburger toggled={isOpen} toggle={setOpen} />
+          </div>
+
+          <div className="menu-items ">
+            <div>
+              <Link to="/">.me()</Link>
+            </div>
+            <div>
+              <Link to="/experience">.experience()</Link>
+            </div>
+            <div>
+              <Link to="/contact">.contact()</Link>
+            </div>
+          </div>
         </div>
-        <div className="menu-items">
-          <div>
-            <Link to="/">About</Link>
-          </div>
-          <div>
-            <Link to="/experiance">Experiance</Link>
-          </div>
-          <div>
-            <Link to="/contact">Contact</Link>
-          </div>
-        </div>
+
+        {isOpen && hamburgerMenu}
       </div>
     </div>
   );
