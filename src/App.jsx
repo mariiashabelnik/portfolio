add393be3b6573c1936988e1f2644750cb26b468
Mariia Shabelnik <>
2022-07-27 13:42:38 +0200

    created routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
